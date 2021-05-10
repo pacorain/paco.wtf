d84548a9474b52d6e1bd88797f69f91188cb4c2b
Austin Rainwater <paco@heckin.io>
2021-05-09 19:00:25 -0500

    Set up basic theme. Not sure I'll change it.

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,29 +4,13 @@
 AUTHOR = 'Austin "Paco" Rainwater'
 SITENAME = 'paco, wtf'
 SITEURL = ''
+THEME = 'themes/basic'
 
 PATH = 'content'
 
 TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = 'en'
-
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
